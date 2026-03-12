94bfccf4d89e5c7f4c5c8e69336adf2666ea6098
Kenner <kennerskindle@gmail.com>
2026-03-12 16:27:56 -0400

    Added more comments for context

--- Workflow_Template.py
+++ Workflow_Template.py
@@ -2,6 +2,7 @@ import sys
 import os
 import subprocess
 import time
+from enum import Enum, auto
 
 
 # base folder for all referenced scripts
@@ -9,57 +10,69 @@ current_dir = os.path.dirname(__file__)
 automation_dir = os.path.abspath(os.path.join(current_dir, ".."))
 sys.path.append(automation_dir)
 
-# all scripts to be used in the workflow
-# from FACTS4DEMES import facts4demes
+# list of task
+task_list = []
 
-# from GP_Export import Daily_GP_Export
+# class of all Status states
+class TaskState(Enum):
+    PENDING = auto()
+    RUNNING = auto()
+    PASSED = auto()
+    FAILED = auto()
 
-# from ONM_Export import ONM_SC
-
-
-workweek_task = []
-# weekend_task = []
 
 # use this class to define a new function
 class Function:
     def __init__(self,name,dependencies,script):
         self.name = name
         self.dependencies = dependencies
-        self.status = "Pending"
+        self.status = TaskState.PENDING
         self.script = script
+        self.processor = None
 
-    def updateStatus(self,new_status):
-        if new_status == "Failed":
-            self.status = new_status
-            print(f"{self.name} has marked as Failed")
-        elif new_status == "Passed":
-            self.status = new_status
-            print(f"{self.name} has marked as Passed")
-        else: 
-            self.status = "Error"
-            print("invalid status selected")
-
-    def check_deps(self):
+    def can_run(self):
         for dep in self.dependencies:
-            if dep.status == "Pending":
-                print(f"{self.name} has been skipped for pending {dep.name}")
+            if dep.status == TaskState.PENDING or dep.status == TaskState.RUNNING:
+                print(rf"{self.name} the dependancy {dep.name} is not ready")
                 return False
-            if dep.status == "Failed" or dep.status == "Error":
-                self.updateStatus("Failed")
-                return False
-
+            if dep.status == TaskState.FAILED:
+                self.status = TaskState.FAILED
+                print(rf"{self.name} can not run. {dep.name} has failed")    
+        print(f"dependancies for {self.name} are all complete.")
         return True
+
+    def start(self):
+        print("attempting start")
+        if self.status == TaskState.PENDING:
+            return
+        self.processor = subprocess.Popen(["sys.executable", self.script])
+        print("process attempted")
+        self.status = TaskState.RUNNING
+        print(f"{self.name} has started")
+
+    def update(self):
+        if self.status != TaskState.RUNNING:
+            return
+        process_status = self.processor.poll()
+        if process_status is None:
+            # this process is still running
+            return
+        elif process_status == 0:
+            self.status = TaskState.PASSED
+        else:
+            self.status = TaskState.FAILED
+            print(f"{self.name} has failed")
 
 # when defining a function provide a string, list of dependancies (any other defined "Function"), a script using importname.__file__ or a file path
 
-from RPA_Import import Daily_RPA_FLPA_Import
-RPA_Import = Function(name="rpa_import", dependencies =[], script = Daily_RPA_FLPA_Import.__file__)
+from GP_Export import Daily_GP_Export
+GP_Export = Function(name="gp_export", dependencies =[], script = Daily_GP_Export.__file__)
 # append task to the list of task
-workweek_task.append(RPA_Import)
+task_list.append(GP_Export)
 
 from FLPA_Export import Daily_FLPA_Exports
-FLPA_Export = Function(name="flpa_export", dependencies =[RPA_Import], script = Daily_FLPA_Exports.__file__)
-workweek_task.append(FLPA_Export)
+FLPA_Export = Function(name="flpa_export", dependencies =[GP_Export], script = Daily_FLPA_Exports.__file__)
+task_list.append(FLPA_Export)
 
 
 # tracking task, uses a dictonary to relate a running process to a task
@@ -68,41 +81,25 @@ current_task= {}
 MAX_TASK = 5
 
 # main processed used to handle the entire workflow
-def run_task():
-    # if there are any task that are waiting or task that are running: keep looping
-    while len(workweek_task) > 0 or len(current_task) > 0:
-        finished_task =[]
-
-        # for task in a COPY of the list of task.
-        for task in list(workweek_task):
-            # remove any task that has been marked Pass/Fail/Error and move on
-            if task.status != "Pending":
-                workweek_task.remove(task)
-                continue
-            # have the dependancies completed? True/False
-            task_ready = task.check_deps()
-
-            # if task_ready is True and there is less than 5 task running, start task.
-            if task_ready and len(current_task) < MAX_TASK:
-                process = subprocess.Popen([sys.executable,task.script])
-                current_task[task] = process
-                workweek_task.remove(task)
+def run_workflow(task_list):
+    while any(t.status in [TaskState.PENDING, TaskState.RUNNING] for t in task_list):
         
-        #check in on all current running task and get their current status
-        for task,process in current_task.items():
-            status_check = process.poll()
-            if status_check is not None:
-                if status_check == 0:
-                    task.updateStatus("Passed")
-                elif status_check != 0:
-                    task.updateStatus("Failed")
-                
-                finished_task.append(task)
+        
+        for task in list(task_list):
+            if task.status == TaskState.PENDING and task.can_run():
+                if len(current_task) < MAX_TASK:
+                    task.start()
+                    current_task[task] = task.processor
 
-        # close out any task that were marked complete in previous section
-        for task in finished_task:
+        finished = []
+        for task in current_task:
+            task.update()
+            if task.status != TaskState.RUNNING:
+                finished.append(task)
+    
+        for task in finished:
             del current_task[task]
-
+    
         time.sleep(1)
 
-run_task()
+run_workflow(task_list)
